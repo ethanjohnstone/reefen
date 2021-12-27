8203c1c7a4012319d4f79779f27dfbf42a0dc448
Ethan <ethanjohnstone0911@gmail.com>
2021-12-27 22:37:21 +1300

    Updated hero img

--- src/components/heros/HeroWithImage.tsx
+++ src/components/heros/HeroWithImage.tsx
@@ -93,7 +93,7 @@ export const HeroWithImage = () => {
                             w={'100%'}
                             h={'100%'}
                             src={
-                                'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
+                                'https://i.imgur.com/aBaQ1HD.jpg'
                             }
                         />
                     </Box>
