f8805382d0abea21bf79242072a495ebe15244ca
Ethan <ethanjohnstone0911@gmail.com>
2021-12-27 21:52:28 +1300

    Added chalice img

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -3,9 +3,9 @@ import { Product } from "./Product";
 
 const productData = [{
     imageURL:
-        'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
-    name: 'Wayfarer Classic 1 ',
-    price: 4.5,
+        `${process.env.PUBLIC_URL}/img/261432726_215677364062716_4462849518609240088.png`,
+    name: 'Golden eye, Green Chalice',
+    price: 150,
     id: 1
 },
 {
